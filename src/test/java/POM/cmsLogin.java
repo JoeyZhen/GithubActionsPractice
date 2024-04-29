d93e006c583cac407e26eb97c72b75efa7690551
Joey Zhen <joey@fluz.app>
2024-04-29 01:16:51 -0400

    add files

---
+++ src/test/java/POM/cmsLogin.java
@@ -0,0 +1,54 @@
+package POM;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class cmsLogin {
+    WebDriver driver;
+    By loginButton = By.xpath("//a[normalize-space()='Login with Google']");
+    By inputUsername = By.id("identifierId");
+    By inputPassword = By.xpath("//input[@name='Passwd']");
+    By nextButton = By.xpath("//span[normalize-space()='Next']");
+    By appIntegrationTab = By.xpath("//a[normalize-space()='Apps and integrations']");
+    By merchantRateTab = By.partialLinkText("Merchant rates testing");
+    By searchMerchant = By.xpath("//input[@aria-autocomplete='list']");
+    By sumbitButton = By.cssSelector("button[class='btn btn-success']");
+
+    public cmsLogin(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void searchGoogle() throws InterruptedException {
+        driver.findElement(loginButton).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(500));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(inputUsername));
+        driver.findElement(inputUsername).sendKeys("doppler-contributor@fluz.app");
+        driver.findElement(nextButton).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(inputPassword));
+        driver.findElement(inputPassword).sendKeys("wiwwK5Zhc[7<VWAI]<RH");
+        driver.findElement(nextButton).click();
+    }
+
+    public void searchMerchantRate() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(500));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(appIntegrationTab));
+        driver.findElement(appIntegrationTab).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(merchantRateTab));
+        driver.findElement(merchantRateTab).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(searchMerchant));
+        driver.findElement(searchMerchant).sendKeys("Amazon");
+
+        Actions actions = new Actions(driver);
+        actions.sendKeys(driver.findElement(searchMerchant), Keys.ENTER).perform();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(sumbitButton));
+        driver.findElement(sumbitButton).click();
+    }
+
+
+}
---
+++ src/test/java/Tests/seleniumTest.java
@@ -0,0 +1,42 @@
+package Tests;
+import POM.cmsLogin;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Starting Test On Chrome Browser");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe");
+        String baseUrl = "https://cms.staging.fluzapp.com";
+        driver = new FirefoxDriver();
+        driver.get(baseUrl);
+        driver.manage().window().maximize();
+    }
+
+//    @Test
+//    public void Login()  throws InterruptedException{
+//          cmsLogin c = new cmsLogin(driver);
+//          c.searchGoogle();
+//          String testTitle = "Tools QA";
+//          String originalTitle = driver.getTitle();
+//          Assert.assertEquals(originalTitle, testTitle);
+//    }
+
+    @Test
+    public void clickRateTab()  throws InterruptedException{
+        cmsLogin c = new cmsLogin(driver);
+        c.searchGoogle();
+        c.searchMerchantRate();
+    }
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+        System.out.println("Finished Test On Chrome Browser");
+    }
+}
